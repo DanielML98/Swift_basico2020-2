7553dda9ff3c8f62558ddf33be780c1e175606ca
DanielML98 <60166480+DanielML98@users.noreply.github.com>
2020-01-23 17:32:10 -0600

    Add files via upload

---
+++ GuessTheFlag_finished/GuessTheFlag_finished/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  GuessTheFlag_finished
+//
+//  Created by Daniel Alejandro on 22/01/20.
+//  Copyright © 2020 DAML. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    var countries: [String] = []
+    var score = 0
+    var correctAnswer = 0
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        askQuestion()
+    }
+    
+    func askQuestion(action: UIAlertAction! = nil){
+        countries.shuffle()
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        correctAnswer = Int.random(in: 0...2)
+        title = countries[correctAnswer].uppercased()
+    }
+   
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        if sender.tag == correctAnswer{
+            title = "Correct"
+            score += 1
+        }else{
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
+    }
+    
+
+}
+
