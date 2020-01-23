8d7a3d13e0d705824ba92f1a5beade7a1b6539e9
DanielML98 <60166480+DanielML98@users.noreply.github.com>
2020-01-23 17:30:37 -0600

    Add files via upload

--- GuessTheFlag/GuessTheFlag/ViewController.swift
+++ GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -17,6 +17,10 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var button3: UIButton!
     
+    var correctAnswer: Int = 0
+    
+    var score: Int = 0
+    
     //var countries: [String] = [] Declaración alternativa de un arreglo de strings que inicia vacío
     var countries = [String]()
 
@@ -35,15 +39,29 @@ class ViewController: UIViewController {
         askQuestion()
     }
     
-    func askQuestion(){
+    func askQuestion(action: UIAlertAction! = nil){
         countries.shuffle()
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
-        
+        correctAnswer = Int.random(in: 0...2)
+        title = countries[correctAnswer].uppercased()
     }
 
-    @IBAction func buttonTapped(_ sender: Any) {
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        if sender.tag == correctAnswer{
+            title = "Correct"
+            score += 1
+            //askQuestion()
+        }else{
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
         
     }
     
